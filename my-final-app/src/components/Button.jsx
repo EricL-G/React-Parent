679edc24fc6e03ebf4e0c0c5b5341bf7ab85bdd1
EricL-G <cheokkuan.leong@gmail.com>
2026-04-03 23:23:20 -0700

    Complete!!

--- my-final-app/src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-function Button ({name}) {
-  return (
-    <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded-full">
-    {name}
-  </button>
-  );
-}
-
-export default Button;
