349a81057d09fe0950db77f2a249881a1c7c62a6
EricL-G <cheokkuan.leong@gmail.com>
2026-04-03 23:26:47 -0700

    Complete!!!

--- my-final-app/src/components/Pictures.jsx
+++ my-final-app/src/components/Pictures.jsx
@@ -8,18 +8,3 @@ function Pictures({ pics }) {
 }
 export default Pictures;
 
-
-
-// import { Carousel } from "flowbite-react";
-
-// export default function Carousels({ pics, pics2, pics3 }) {
-//   return (
-//     <div className="w-full mx-auto my-10 px-12" style={{ maxWidth: '500px', height: '300px' }}>
-//       <Carousel>
-//         <img src={pics} alt="..." />
-//         <img src={pics2} alt="..." />
-//         <img src={pics3} alt="..." />
-//       </Carousel>
-//     </div>
-//   );
-// }
