66c576d6c024c7b4b19aa16cf8c9126325b77c3c
EricL-G <cheokkuan.leong@gmail.com>
2026-04-03 23:30:04 -0700

    FinalComplete

--- my-final-app/src/App.jsx
+++ my-final-app/src/App.jsx
@@ -1,14 +1,3 @@
-// import Header from './components/Header';
-// import List from './components/List';
-// // import Imageslide from './components/Imageslide'
-// import Button from './components/Button'
-// import CanvasSwitcher from './components/CanvasSwitcher'
-// import myPics from './assets/Albert3.png';
-// import myPics2 from './assets/DebtCategories.png';
-// import myPics3 from './assets/usdebt.png';
-// import React from "react";
-// import Carousels from "./components/Carousels";
-// import Radio from "./components/Radio";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import First from "./pages/First";
 import Second from "./pages/Second";
