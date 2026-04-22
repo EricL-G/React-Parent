feae911b283ca16cabf07ffcf0897de8d0afcbaa
EricL-G <cheokkuan.leong@gmail.com>
2026-04-22 10:10:15 -0700

    WorkingNow

--- my-final-app/src/pages/Final.jsx
+++ my-final-app/src/pages/Final.jsx
@@ -1,11 +1,9 @@
 import CanvasSwitcher from '../components/CanvasSwitcher'
-import Advice from '../components/Advice'
 function Final() {
  return (
  <>
    <h3>by Cheok Kuan Leong</h3>
    <CanvasSwitcher />
-   <Advice />
    <h2>For more information, please email Businessforce@gmail.com</h2>
  </>
 )
