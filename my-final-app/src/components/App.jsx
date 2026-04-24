138eeea41264be287195bd2a1631cf7dc10b79bf
EricL-G <cheokkuan.leong@gmail.com>
2026-04-24 09:48:27 -0700

    Check

--- my-final-app/src/components/App.jsx
+++ my-final-app/src/components/App.jsx
@@ -1,26 +1,28 @@
-
+import React from "react";
 import { Button, Checkbox, Label, TextInput } from "flowbite-react";
 
-export function Component() {
+export default function App() {
+    const handleClick = () => {
+    alert("Submitted!");
+  };
   return (
-    <form className="flex max-w-md flex-col gap-4">
+    <form className="flex max-w-md flex-col gap-4 justify-center items-center text-center m-8 ml-30 p-8 border-2 border-gray-300 rounded-lg shadow-lg">
       <div>
         <div className="mb-2 block">
           <Label htmlFor="email1">Your email</Label>
         </div>
-        <TextInput id="email1" type="email" placeholder="name@flowbite.com" required />
-      </div>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="password1">Your password</Label>
-        </div>
-        <TextInput id="password1" type="password" required />
+        <TextInput id="email1" type="email" placeholder="name@gmail.com" required />
       </div>
       <div className="flex items-center gap-2">
         <Checkbox id="remember" />
         <Label htmlFor="remember">Remember me</Label>
       </div>
-      <Button type="submit">Submit</Button>
+      <Button 
+      type="button" 
+      className="bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded border-2 border-red-700 transition-all duration-300 hover:scale-105 transition-transform duration-300"
+      onClick={handleClick}>
+        Submit
+      </Button>
     </form>
   );
 }
--- my-final-app/src/pages/Final.jsx
+++ my-final-app/src/pages/Final.jsx
@@ -1,9 +1,11 @@
 import CanvasSwitcher from '../components/CanvasSwitcher'
+import App from '../components/App'
 function Final() {
  return (
  <>
    <h3>by Cheok Kuan Leong</h3>
    <CanvasSwitcher />
+   <App />
    <h2>For more information, please email Businessforce@gmail.com</h2>
  </>
 )
